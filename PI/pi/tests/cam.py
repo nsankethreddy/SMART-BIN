6b25f7f2cfb5d468d0d4a5838d57661aeafcf529
Abhishek <Abhishek262014@gmail.com>
2020-03-02 01:57:43 +0530

    Update cam.py

--- PI/pi/tests/cam.py
+++ PI/pi/tests/cam.py
@@ -1,6 +1,7 @@
 from picamera import PiCamera
 from time import sleep
 import subprocess
+import datetime
 
 def captureImage():
 
@@ -15,3 +16,13 @@ def captureImage():
 captureImage()
 
 subprocess.run(["scp", "/home/pi/Desktop/image.jpg", "user@192.168.43.108:/home/user"])
+
+dt = datetime.datetime.now().strftime('%d-%m-%Y %H:%M:%S')
+dateStr,timeStr = dt.split()
+
+path_to_pic = "/home/user/Checked/"+dateStr+"_"+timeStr+".jpg"
+with open("/home/pi/Desktop/d.txt","w+") as dataFile : 
+  dataFile.write(dateStr+" " +timeStr)
+
+subprocess.run(["scp", "/home/pi/Desktop/image.jpg", "user@192.168.43.108:/home/user/Checked/"+dateStr+"_"+timeStr+".jpg"])
+subprocess.run(["scp", "/home/pi/Desktop/d.txt", "user@192.168.43.108:/home/user/Desktop"])
