5a8318ce9a8c4a0e5643242d9029154c701b9e4d
skywalker60 <42914419+skywalker60@users.noreply.github.com>
2020-03-01 14:35:25 +0530

    Add files via upload

---
+++ CLIENT/smart_bin/lib/login.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginPage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+
+class LoginPage extends StatefulWidget {
+  LoginPage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+  @override
+
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          children: <Widget>[
+            SizedBox(height: 80.0),
+            Column(
+              children: <Widget>[
+                Image.asset('assets/icon.jpeg'),
+                SizedBox(height: 20.0),
+                Text('login bois'),
+              ],
+            ),
+            SizedBox(height: 120.0),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'username',
+                filled: true,
+              ),
+            ),
+            SizedBox(height: 12.0),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'password',
+                filled: true,
+              ),
+              obscureText: true,
+            ),
+            ButtonBar(
+              children: <Widget>[
+                FlatButton(
+                  child: Text('Cancel'),
+                  onPressed: null,
+                ),
+                RaisedButton(
+                  child: Text('next'),
+                  onPressed: null,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+    throw UnimplementedError();
+  }
+}
