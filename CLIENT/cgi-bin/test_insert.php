6e1ba9e965047aba80417334a37d0e139b6a267b
Gituser143 <bhargavsnv100@gmail.com>
2020-03-02 06:39:12 +0530

    Test register

---
+++ CLIENT/cgi-bin/test_insert.php
@@ -0,0 +1,25 @@
+<?php
+// include_once('addUser.php');
+// include('connect/mysqli_connect.php');
+session_start();
+
+$errors = array();
+$username = "";
+$uname = "root";
+$password = "Reaper42@";
+$database = "smart_bins";
+$mysqli = new mysqli("localhost", $uname, $password, $database);
+if ($mysqli -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
+}
+$query = "INSERT INTO account VALUES(NULL,"uname","pass")";
+if ($result = $mysqli->query($query))
+{
+  echo "Success";
+}
+else {
+  echo "Failed";
+}
+
+?>
